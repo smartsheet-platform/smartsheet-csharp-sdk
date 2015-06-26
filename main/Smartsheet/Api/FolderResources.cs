19522808a08dd1b44a542b677c79a47207397926
EricYan1 <ericyan1@outlook.com>
2015-06-25 18:03:45 -0700

    Updated FolderResources interface to API2
    Will commit on a Task by task (endpoint by endpoint) basis in next
    sprint.

--- main/Smartsheet/Api/FolderResources.cs
+++ main/Smartsheet/Api/FolderResources.cs
@@ -23,6 +23,7 @@ namespace Smartsheet.Api
 
 
 	using Folder = Api.Models.Folder;
+	using ObjectInclusion = Api.Models.ObjectInclusion;
 
 	/// <summary>
 	/// <para>This interface provides methods To access Folder resources.</para>
@@ -33,11 +34,11 @@ namespace Smartsheet.Api
 	{
 
 		/// <summary>
-		/// <para>Get a folder.</para>
-		/// 
-		/// <para>It mirrors To the following Smartsheet REST API method: GET /folder/{Id}</para>
+		/// <para>Gets the specified Folder (and lists its contents).</para>
+		/// <para>It mirrors To the following Smartsheet REST API method: GET /folders/{folderId}</para>
 		/// </summary>
 		/// <param name="folderId"> the folder Id </param>
+		/// <param name="include"> (optional) – comma-separated list of elements to include in the respons</param>
 		/// <returns> the folder (note that if there is no such resource, this method will throw ResourceNotFoundException 
 		/// rather than returning null) </returns>
 		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
@@ -46,10 +47,11 @@ namespace Smartsheet.Api
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
 		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-		Folder GetFolder(long folderId);
+		Folder GetFolder(long folderId, IEnumerable<ObjectInclusion> include);
 
 		/// <summary>
-		/// <para>Update a folder.</para>
+		/// <para>Updates a folder.</para>
+		/// <para>It mirrors To the following Smartsheet REST API method: PUT /folders/{folderId}</para>
 		/// </summary>
 		/// <param name="folder"> the folder To update </param>
 		/// <returns> the updated folder (note that if there is no such folder, this method will throw Resource Not Found 
@@ -63,10 +65,9 @@ namespace Smartsheet.Api
 		Folder UpdateFolder(Folder folder);
 
 		/// <summary>
-		/// <para>Delete a folder.</para>
-		/// 
+		/// <para>Deletes a folder.</para>
 		/// <para>It mirrors To the following Smartsheet REST API method:<br />
-		/// DELETE /folder{Id}</para>
+		/// DELETE /folders/{folderId}</para>
 		/// </summary>
 		/// <param name="folderId"> the folder Id </param>
 		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
@@ -78,11 +79,11 @@ namespace Smartsheet.Api
 		void DeleteFolder(long folderId);
 
 		/// <summary>
-		/// <para>List child Folders of a given folder.</para>
-		/// 
-		/// <para>It mirrors To the following Smartsheet REST API method:<br /> GET /folder/{Id}/Folders</para>
+		/// <para>Gets a list of the top-level child Folders within the specified Folder.</para>
+		/// <remarks>This operation supports pagination of results. For more information, see Paging.</remarks>
+		/// <para>It mirrors To the following Smartsheet REST API method:<br /> GET /folders/{folderId}/folders</para>
 		/// </summary>
-		/// <param name="parentFolderId"> the parent folder Id </param>
+		/// <param name="folderId"> the folderId</param>
 		/// <returns> the child Folders (note that an empty list will be returned if no child folder is found). </returns>
 		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
@@ -90,15 +91,14 @@ namespace Smartsheet.Api
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
 		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-		IList<Folder> ListFolders(long parentFolderId);
+		IList<Folder> ListFolders(long folderId, bool includeAll, long? pageSize, long? page);
 
 		/// <summary>
-		/// <para>Create a folder.</para>
-		/// 
+		/// <para>Creates a Folder in the specified Folder.</para>
 		/// <para>It mirrors To the following Smartsheet REST API method:<br />
-		/// POST /folder/{Id}/Folders</para>
+		/// POST /folders/{folderId}/folders</para>
 		/// </summary>
-		/// <param name="parentFolderId"> the parent folder Id </param>
+		/// <param name="folderId"> the parent folder Id </param>
 		/// <param name="folder"> the folder To create </param>
 		/// <returns> the created folder </returns>
 		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
@@ -107,7 +107,7 @@ namespace Smartsheet.Api
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
 		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-		Folder CreateFolder(long parentFolderId, Folder folder);
+		Folder CreateFolder(long folderId, Folder folder);
 	}
 
 }
