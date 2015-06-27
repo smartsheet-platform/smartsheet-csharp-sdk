64f2f160baa08c11f9c8a6f38ee2afa9a63cc188
EricYan1 <ericyan1@outlook.com>
2015-06-26 17:47:30 -0700

    Added PaginationParameter from Java SDK

---
+++ main/Smartsheet/Api/Models/PaginationParameters.cs
@@ -0,0 +1,61 @@
+﻿using Smartsheet.Api.Internal.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smartsheet.Api.Models
+{
+	public class PaginationParameters
+	{
+		private bool includeAll;
+
+		private int? pageSize;
+
+		private int? page;
+
+		public PaginationParameters(bool includeAll, int? pageSize, int? page)
+		{
+			this.includeAll = includeAll;
+			this.pageSize = pageSize;
+			this.page = page;
+		}
+
+		public bool IncludeAll
+		{
+			get { return includeAll; }
+			set { includeAll = value; }
+		}
+		public int? PageSize
+		{
+			get { return pageSize; }
+			set { pageSize = value; }
+		}
+		public int? Page
+		{
+			get { return page; }
+			set { page = value; }
+		}
+
+
+		public string ToQueryString()
+		{
+			Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+			parameters.Add("includeAll", includeAll.ToString());
+
+			if (!includeAll)
+			{
+				if (pageSize.HasValue)
+				{
+					parameters.Add("pageSize", pageSize.ToString());
+				}
+				if (page.HasValue)
+				{
+					parameters.Add("page", page.ToString());
+				}
+			} 
+			return QueryUtil.GenerateUrl(null, parameters);
+		}
+	}
+}
