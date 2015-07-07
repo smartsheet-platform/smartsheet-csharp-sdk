09bb7a8936387a90b41e21aabec1033fe588576d
EricYan1 <ericyan1@outlook.com>
2015-07-06 17:54:13 -0700

    New DataWrapper model

---
+++ main/Smartsheet/Api/Models/DataWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Smartsheet_csharp_sdk.main.Smartsheet.Api.Models
+{
+	class DataWrapper
+	{
+	}
+}
