c0f2975821a113c7e833264d8868e845a1453e84
EricYan1 <ericyan1@outlook.com>
2015-07-15 11:52:48 -0700

    Update to DiscussionCommentResources stub classes

--- main/Smartsheet/Api/Internal/DiscussionCommentResourcesImpl.cs
+++ main/Smartsheet/Api/Internal/DiscussionCommentResourcesImpl.cs
@@ -4,9 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Smartsheet_csharp_sdk.main.Smartsheet.Api.Internal
+namespace Smartsheet.Api.Internal
 {
 	public class DiscussionCommentResourcesImpl : AbstractResources, DiscussionCommentResources
 	{
+		/// <summary>
+		/// Constructor.
+		/// 
+		/// Exceptions: - IllegalArgumentException : if any argument is null
+		/// </summary>
+		/// <param name="smartsheet"> the Smartsheet </param>
+		public DiscussionCommentResourcesImpl(SmartsheetImpl smartsheet)
+			: base(smartsheet)
+		{ }
 	}
 }
