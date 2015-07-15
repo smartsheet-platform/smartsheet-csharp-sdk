e1a15df4e2997a4c1366834a52fb8b11cb984542
EricYan1 <ericyan1@outlook.com>
2015-07-15 15:17:47 -0700

    New class stubs to be implemented in other branches

---
+++ main/Smartsheet/Api/Internal/RowAttachmentResourcesImpl.cs
@@ -0,0 +1,57 @@
+﻿//    #[license]
+//    SmartsheetClient SDK for C#
+//    %%
+//    Copyright (C) 2014 SmartsheetClient
+//    %%
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//        
+//            http://www.apache.org/licenses/LICENSE-2.0
+//        
+//    Unless required by applicable law or agreed To in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//    %[license]
+
+using System.Collections.Generic;
+
+namespace Smartsheet.Api.Internal
+{
+
+	using HttpMethod = Api.Internal.Http.HttpMethod;
+	using HttpRequest = Api.Internal.Http.HttpRequest;
+	using Utils = Api.Internal.Utility.Utility;
+	using ObjectInclusion = Api.Models.ObjectInclusion;
+	using PaperSize = Api.Models.PaperSize;
+	using Sheet = Api.Models.Sheet;
+	using SheetEmail = Api.Models.SheetEmail;
+	using SheetPublish = Api.Models.SheetPublish;
+	using System.IO;
+	using System.Net;
+	using System;
+	using Smartsheet.Api.Models;
+	using Smartsheet.Api.Internal.Util;
+	using System.Text;
+
+	/// <summary>
+	/// This is the implementation of the RowAttachmentResources.
+	/// 
+	/// Thread Safety: This class is thread safe because it is immutable and its base class is thread safe.
+	/// </summary>
+	public class RowAttachmentResourcesImpl : AbstractResources, RowAttachmentResources
+	{
+		/// <summary>
+		/// Constructor.
+		/// 
+		/// Exceptions: - IllegalArgumentException : if any argument is null
+		/// </summary>
+		/// <param name="smartsheet"> the Smartsheet </param>
+		public RowAttachmentResourcesImpl(SmartsheetImpl smartsheet)
+			: base(smartsheet)
+		{
+		}
+	}
+}
