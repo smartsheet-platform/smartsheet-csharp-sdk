ab128ab9c1bcbe1e1de53d72d307108bf27cb638
EricYan1 <ericyan1@outlook.com>
2015-07-15 11:45:41 -0700

    Revert "csproj for compilation"
    This reverts commit 8006ac93c955353ced5dc4455026d87ee7fb4e22.

--- main/Smartsheet/Api/DiscussionCommentResources..cs
+++ main/Smartsheet/Api/DiscussionCommentResources..cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Smartsheet.Api
+namespace Smartsheet_csharp_sdk.main.Smartsheet.Api
 {
 	public interface DiscussionCommentResources
 	{
--- main/Smartsheet/Api/Internal/DiscussionCommentResourcesImpl.cs
+++ main/Smartsheet/Api/Internal/DiscussionCommentResourcesImpl.cs
@@ -4,18 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Smartsheet.Api.Internal
+namespace Smartsheet_csharp_sdk.main.Smartsheet.Api.Internal
 {
 	public class DiscussionCommentResourcesImpl : AbstractResources, DiscussionCommentResources
 	{
-		/// <summary>
-		/// Constructor.
-		/// 
-		/// Exceptions: - IllegalArgumentException : if any argument is null
-		/// </summary>
-		/// <param name="smartsheet"> the Smartsheet </param>
-		public DiscussionCommentResourcesImpl(SmartsheetImpl smartsheet)
-			: base(smartsheet)
-		{ }
 	}
 }
