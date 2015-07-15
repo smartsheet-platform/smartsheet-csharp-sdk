2191c1051f3e2c8e4b5f687a79277ca63d39a121
EricYan1 <ericyan1@outlook.com>
2015-07-15 10:23:52 -0700

    Updated DiscussionCommentResourcesImpl Test, still work in progress, new json responses

--- TestSDK/Smartsheet/Api/Internal/DiscussionCommentResourcesImplTest.cs
+++ TestSDK/Smartsheet/Api/Internal/DiscussionCommentResourcesImplTest.cs
@@ -18,13 +18,29 @@
 					"accessToken", new DefaultHttpClient(), serializer));
 		}
 
+
 		[Test]
 		public virtual void TestAddCommentWithAttachment()
 		{
 			string file = @"..\..\..\TestSDK\resources\wordFile.docx";
-			Comment comment = new Comment.AddCommentBuilder().SetText("sddfhghjhjgherdh AAAA").Build();
+			Comment comment = new Comment.AddCommentBuilder().SetText("This is a new comment.").Build();
+			server.setResponseBody("../../../TestSDK/resources/addCommentWithAttachment.json");
+			commentResources.AddCommentWithAttachment(8357140688594820, 5773448686397316, comment, file, "application/msword");
+
+			//Will not be able to deserialize (wIll throw error) unless Attachment object is also updated.
+			//Assset TO-DO
+		}
+
+		[Test]
+		public virtual void TestAddComment()
+		{
+			Comment comment = new Comment.AddCommentBuilder().SetText("This is a new comment.").Build();
 			server.setResponseBody("../../../TestSDK/resources/addComment.json");
-			commentResources.AddComment(8357140688594820, 5773448686397316, comment, file, "application/msword");
+			comment = commentResources.AddComment(8357140688594820, 5773448686397316, comment);
+
+			Assert.IsTrue(comment.CreatedBy.Email == "john.doe@smartsheet.com");
+			Assert.IsTrue(comment.ID == 6834973207488388);
+			Assert.IsTrue(comment.Text == "This is a new comment.");
 
 		}
 	}
