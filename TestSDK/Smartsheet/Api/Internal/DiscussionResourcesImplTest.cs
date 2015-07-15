fd037e5f992a713a79819d1ef93e7d96f6df6eeb
EricYan1 <ericyan1@outlook.com>
2015-07-15 10:05:40 -0700

    Commented out DiscussionResourcesImplTest

--- TestSDK/Smartsheet/Api/Internal/DiscussionResourcesImplTest.cs
+++ TestSDK/Smartsheet/Api/Internal/DiscussionResourcesImplTest.cs
@@ -8,35 +8,35 @@ namespace Smartsheet.Api.Internal
 	public class DiscussionResourcesImplTest : ResourcesImplBase
 	{
 
-		private DiscussionResourcesImpl discussionResources;
+		//private DiscussionResourcesImpl discussionResources;
 
-		[SetUp]
-		public virtual void SetUp()
-		{
-			discussionResources = new DiscussionResourcesImpl(new SmartsheetImpl("http://localhost:9090/1.1/", 
-					"accessToken", new DefaultHttpClient(), serializer));
-		}
+		//[SetUp]
+		//public virtual void SetUp()
+		//{
+		//	discussionResources = new DiscussionResourcesImpl(new SmartsheetImpl("http://localhost:9090/1.1/", 
+		//			"accessToken", new DefaultHttpClient(), serializer));
+		//}
 
-		[Test]
-		public virtual void TestDiscussionResourcesImpl()
-		{
-		}
+		//[Test]
+		//public virtual void TestDiscussionResourcesImpl()
+		//{
+		//}
 
-		[Test]
-		public virtual void TestGetDiscussion()
-		{
-			server.setResponseBody("../../../TestSDK/resources/getDiscussion.json");
+		//[Test]
+		//public virtual void TestGetDiscussion()
+		//{
+		//	server.setResponseBody("../../../TestSDK/resources/getDiscussion.json");
 
-			Discussion discussion = discussionResources.GetDiscussion(1234L);
+		//	Discussion discussion = discussionResources.GetDiscussion(1234L);
 
-			Assert.AreEqual("New Discussion", discussion.Title);
-			Assert.NotNull(discussion.Comments);
-			Assert.True(discussion.Comments.Count == 3);
-			Assert.AreEqual("This text is the body of the first comment4", discussion.Comments[0].Text);
-			Assert.NotNull(discussion.Comments[0].CreatedBy);
-			Assert.AreEqual("Brett Batie", discussion.Comments[0].CreatedBy.Name);
-			Assert.AreEqual("email@email.com", discussion.Comments[0].CreatedBy.Email);
-		}
+		//	Assert.AreEqual("New Discussion", discussion.Title);
+		//	Assert.NotNull(discussion.Comments);
+		//	Assert.True(discussion.Comments.Count == 3);
+		//	Assert.AreEqual("This text is the body of the first comment4", discussion.Comments[0].Text);
+		//	Assert.NotNull(discussion.Comments[0].CreatedBy);
+		//	Assert.AreEqual("Brett Batie", discussion.Comments[0].CreatedBy.Name);
+		//	Assert.AreEqual("email@email.com", discussion.Comments[0].CreatedBy.Email);
+		//}
 
 		//[Test]
 		//public virtual void TestAddDiscussionComment()
