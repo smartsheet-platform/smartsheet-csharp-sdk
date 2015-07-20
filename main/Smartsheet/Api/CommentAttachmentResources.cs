daa2e8f847450605d1ea035b0e5248f7dace47d3
EricYan1 <ericyan1@outlook.com>
2015-07-20 12:04:19 -0700

    resolve merge conflicts

--- main/Smartsheet/Api/CommentAttachmentResources.cs
+++ main/Smartsheet/Api/CommentAttachmentResources.cs
@@ -24,6 +24,47 @@ namespace Smartsheet.Api
 
 	public interface CommentAttachmentResources
 	{
+		/// <summary>
+		/// <para>Attaches a file to the Comment.</para>
+		/// <para>This operation will always create a new attachment.
+		/// To upload a new version of the same attachment, use the Attach New Version operation.</para>
+		/// <para>It mirrors To the following Smartsheet REST API method:
+		/// POST /sheets/{sheetId}/comments/{commentId}/attachments</para>
+		/// </summary>
+		/// <param name="sheetId"> the sheetId </param>
+		/// <param name="commentId"> the comment Id </param>
+		/// <param name="file"> the file path </param>
+		/// <param name="fileType"> the file type </param>
+		/// <returns> the newly created Attachment </returns>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
+		Attachment AttachFile(long sheetId, long commentId, string file, string fileType);
 
+		/// <summary>
+		/// <para>Attaches a URL to the Comment.</para>
+		/// <para>It mirrors To the following Smartsheet REST API method:
+		/// POST /sheets/{sheetId}/comments/{commentId}/attachments</para>
+		/// <remarks><para>attachmentSubType is valid only for GOOGLE_DRIVE attachments which are Google Docs.
+		/// It can optionally be included to indicate the type of a file.
+		/// The following attachmentSubTypes are valid for GOOGLE_DRIVE attachments "DOCUMENT", "SPREADSHEET", "PRESENTATION", "PDF", "DRAWING".</para>
+		/// <para>When the attachment type is BOX_COM, DROPBOX, or GOOGLE_DRIVE (without an attachmentSubType specified),
+		/// the mimeType will be derived by the file extension specified on the “name”.</para>
+		/// </remarks>
+		/// </summary>
+		/// <param name="sheetId"> the sheetId </param>
+		/// <param name="commentId"> the comment Id </param>
+		/// <param name="attachment"> the attachment object </param>
+		/// <returns> the newly created Attachment </returns>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
+		Attachment AttachUrl(long sheetId, long commentId, Attachment attachment);
 	}
 }
