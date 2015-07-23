f4e7fd948704b2b9015b40b914ccd509fee57b0c
EricYan1 <ericyan1@outlook.com>
2015-07-22 18:14:00 -0700

    comments

--- main/Smartsheet/Api/SheetCommentResources.cs
+++ main/Smartsheet/Api/SheetCommentResources.cs
@@ -16,17 +16,51 @@
 //    limitations under the License.
 //    %[license]
 
+using Smartsheet.Api.Internal;
+using Smartsheet.Api.Models;
 using System.Collections.Generic;
 
 namespace Smartsheet.Api
 {
 	/// <summary>
 	/// <para>This interface provides methods To access Comment resources that are associated To a sheet object.</para>
-	/// 
 	/// <para>Thread Safety: Implementation of this interface must be thread safe.</para>
 	/// </summary>
 	public interface SheetCommentResources
 	{
+		/// <summary>
+		/// <para>Deletes the Comment specified in the URL.</para>
+		/// <para>It mirrors To the following Smartsheet REST API method: DELETE /sheets/{sheetId}/comments/{commentId}</para>
+		/// </summary>
+		/// <param name="sheetId"> the sheetId </param>
+		/// <param name="commentId">the commentId</param>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
+		void DeleteComment(long sheetId, long commentId);
 
+		/// <summary>
+		/// <para>Gets the Comment specified in the URL.</para>
+		/// <para>It mirrors To the following Smartsheet REST API method: GET /sheets/{sheetId}/comments/{commentId}</para>
+		/// </summary>
+		/// <param name="sheetId">the id of the sheet</param>
+		/// <param name="commentId">the id the of the comment</param>
+		/// <returns> the comment object </returns>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
+		Comment GetComment(long sheetId, long commentId);
+
+		/// <summary>
+		/// Return the CommentAttachmentResources object that provides access To Attachment resources associated with Comment resources.
+		/// </summary>
+		/// <returns> the Attachment resources </returns>
+		CommentAttachmentResources AttachmentResources();
 	}
 }
