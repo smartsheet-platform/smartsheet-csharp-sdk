692fe0f0428b5729b5bcf499d27fe4658f228e76
EricYan1 <ericyan1@outlook.com>
2015-07-28 12:31:46 -0700

    Added commenting to CopyOrMoveRowDirective

--- main/Smartsheet/Api/Models/CopyOrMoveRowDirective.cs
+++ main/Smartsheet/Api/Models/CopyOrMoveRowDirective.cs
@@ -5,18 +5,32 @@ using System.Text;
 
 namespace Smartsheet.Api.Models
 {
+	/// <summary>
+	/// A CopyOrMoveRowDirective object that holds information on the direction of the rows being copied or moved to.
+	/// </summary>
 	public class CopyOrMoveRowDirective
 	{
 		private IList<long> rowIds;
 
 		private CopyOrMoveRowDestination to;
 
+		/// <summary>
+		/// The IDs of the rows to move or copy from the source sheet
+		/// </summary>
+		/// <remarks>
+		/// Up to 5,000 row IDs can be specified in the request,
+		/// but if the total number of rows in the destination sheet after the copy exceeds the Smartsheet row limit,
+		/// an error response will be returned.
+		/// </remarks>
 		public IList<long> RowIds
 		{
 			get { return rowIds; }
 			set { rowIds = value; }
 		}
 
+		/// <summary>
+		/// A CopyOrMoveRowDestination object that identifies the destination sheet
+		/// </summary>
 		public CopyOrMoveRowDestination To
 		{
 			get { return to; }
