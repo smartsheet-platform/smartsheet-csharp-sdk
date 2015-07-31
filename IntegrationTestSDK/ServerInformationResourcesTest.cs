a2c96f600175f1b9e6f939cb024999c2570fc1ba
EricYan1 <ericyan1@outlook.com>
2015-07-31 14:32:42 -0700

    New Server Infromation Integration test

---
+++ IntegrationTestSDK/ServerInformationResourcesTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using System.Collections.Generic;
+
+namespace IntegrationTestSDK
+{
+	[TestClass]
+	public class ServerInformationResourcesTest
+	{
+
+		[TestMethod]
+		public void TestServerInfoResources()
+		{
+			SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken("47ieup4lwsu9nj34j7kitol7nb").Build();
+
+			ServerInfo info = smartsheet.ServerInfoResources().GetServerInfo();
+			Assert.IsTrue(info.FeatureInfo != null);
+			Assert.IsTrue(info.Formats != null);
+			Assert.IsTrue(info.SupportedLocales != null);
+
+		}
+	}
+}
