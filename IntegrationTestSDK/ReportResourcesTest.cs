f8297d45bee5617562e5bc27d049cc165de4924d
EricYan1 <ericyan1@outlook.com>
2015-08-05 10:43:33 -0700

    Must manually add a report id and report name from smarsheet.com

--- IntegrationTestSDK/ReportResourcesTest.cs
+++ IntegrationTestSDK/ReportResourcesTest.cs
@@ -17,13 +17,19 @@ namespace IntegrationTestSDK
 
 			SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).Build();
 
-			//Must manually create a Report at smartsheet.com and paste reportId below.
-			long reportId = 6224034443618180;
+			//Must manually create a Report at smartsheet.com and paste reportId and reportName below.
+			long reportId = 2006874775152516;
+			string reportName = "ReportTest";
 
 			Report report = smartsheet.ReportResources.GetReport(reportId, new ReportInclusion[] { ReportInclusion.ATTACHMENTS, ReportInclusion.DISCUSSIONS }, null, null);
-			Assert.IsTrue(report.Rows.Count > 0);
+			Assert.IsTrue(report.Name == reportName);
 			SheetEmail email = new SheetEmail.CreateSheetEmail(new Recipient[] { new Recipient { Email = "ericyan99@outlook.com" } }, SheetEmailFormat.PDF).SetCcMe(true).Build();
 			smartsheet.ReportResources.SendReport(reportId, email);
+
+			PaginatedResult<Report> reportResults = smartsheet.ReportResources.ListReports(null);
+			Assert.IsTrue(reportResults.Data.Count == 1);
+			Assert.IsTrue(reportResults.Data[0].Name == reportName);
+
 		}
 	}
 }
