a649fa5728425f7a992ab294acd54a9a9ffd2b1b
EricYan1 <ericyan1@outlook.com>
2015-08-06 16:19:53 -0700

    New FolderResourcesCopyTest

---
+++ IntegrationTestSDK/FolderResourcesCopyTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Smartsheet.Api;
+using Smartsheet.Api.Models;
+using System.Configuration;
+
+namespace IntegrationTestSDK
+{
+	[TestClass]
+	public class FolderResourcesCopyTest
+	{
+		[TestMethod]
+		public void TestFolderCopyResources()
+		{
+			string accessToken = ConfigurationManager.AppSettings["testAccessToken"];
+
+			SmartsheetClient smartsheet = new SmartsheetBuilder().SetAccessToken(accessToken).SetBaseURI("https://api.test.smartsheet.com/2.0/").Build();
+			// Before
+			// Folder1-----SubFolder1
+			// Folder2
+
+			// After
+			// Folder1-----SubFolder1
+			// Folder2-----SubFolder1Copy
+			long createdFolderInHomeId1 = CreateFolderInHome(smartsheet, "Folder1");
+			long createdFolderInHomeId2 = CreateFolderInHome(smartsheet, "Folder2");
+
+			long createdFolderInFolderId = CreateFolderInFolder(smartsheet, createdFolderInHomeId1, "SubFolder1");
+
+			ContainerDestination destination = new ContainerDestination
+			{
+				DestinationId = createdFolderInHomeId2,
+				DestinationType = DestinationType.FOLDER,
+				NewName = "SubFolder1Copy"
+			};
+			Folder newCopiedFolder = smartsheet.FolderResources.CopyFolder(createdFolderInFolderId, destination, new FolderCopyInclusion[] { FolderCopyInclusion.ALL }, null);
+
+			Assert.IsTrue(newCopiedFolder.Name == "SubFolder1Copy");
+
+			long copiedFolderId = newCopiedFolder.Id.Value;
+
+			Folder copiedFolder = smartsheet.FolderResources.GetFolder(copiedFolderId, null);
+			Assert.IsTrue(copiedFolder.Name == "SubFolder1Copy");
+
+			DeleteFolders(smartsheet, createdFolderInHomeId1, createdFolderInHomeId2);
+		}
+
+
+		private static void DeleteFolders(SmartsheetClient smartsheet, long folder1, long folder2)
+		{
+			smartsheet.FolderResources.DeleteFolder(folder2);
+			try
+			{
+				smartsheet.FolderResources.GetFolder(folder2, null);
+				Assert.Fail("Exception should have been thrown. Cannot get a deleted folder.");
+			}
+			catch
+			{
+				// Should be "Not Found".
+			}
+			smartsheet.FolderResources.DeleteFolder(folder1);
+			try
+			{
+				smartsheet.FolderResources.GetFolder(folder1, null);
+				Assert.Fail("Exception should have been thrown. Cannot get a deleted folder.");
+			}
+			catch
+			{
+				// Should be "Not Found".
+			}
+		}
+
+		private static long CreateFolderInFolder(SmartsheetClient smartsheet, long createdFolderInHomeId, string folderName)
+		{
+			Folder createdFolderInFolder = smartsheet.FolderResources.CreateFolder(createdFolderInHomeId, new Folder.CreateFolderBuilder(folderName).Build());
+			return createdFolderInFolder.Id.Value;
+		}
+
+		private static long CreateFolderInHome(SmartsheetClient smartsheet, string folderName)
+		{
+			Folder createdFolderInHome = smartsheet.HomeResources.FolderResources.CreateFolder(new Folder.CreateFolderBuilder(folderName).Build());
+			return createdFolderInHome.Id.Value;
+		}
+	}
+}
