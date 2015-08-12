2e55b41a3340b865df811c058dc7353cc6f85f86
EricYan1 <ericyan1@outlook.com>
2015-08-12 15:45:51 -0700

    Reverted back to using Restsharp package from nuget instead of cutom restharp

--- main/Smartsheet/Api/Internal/Http/DefaultHttpClient.cs
+++ main/Smartsheet/Api/Internal/Http/DefaultHttpClient.cs
@@ -125,8 +125,12 @@ namespace Smartsheet.Api.Internal.Http
 			restRequest.AddFile("file", File.ReadAllBytes(file), new FileInfo(file).Name, fileType);
 			if (smartsheetRequest.Entity != null && smartsheetRequest.Entity.GetContent() != null)
 			{
-				restRequest.AddParameter(objectType, System.Text.Encoding.Default.GetString(smartsheetRequest.Entity.Content), "application/json",
-					ParameterType.RequestBody);
+				//Once Restsharp updates their nuget package to properly support multipart upload request, should use the line code
+				// below and comment out the other line. The line of code below should look similar to the new imlpementation in
+				//the new version of Restsharp as I sent a pull request.
+				//restRequest.AddParameter(objectType, System.Text.Encoding.Default.GetString(smartsheetRequest.Entity.Content), "application/json",
+				//	ParameterType.RequestBody);
+				restRequest.AddParameter(objectType, System.Text.Encoding.Default.GetString(smartsheetRequest.Entity.Content), ParameterType.RequestBody);
 			}
 
 			restRequest.AlwaysMultipartFormData = true;
@@ -175,7 +179,7 @@ namespace Smartsheet.Api.Internal.Http
 			Util.ThrowIfNull(smartsheetRequest);
 			if (smartsheetRequest.Uri == null)
 			{
-				 throw new System.ArgumentException("A Request URI is required.");
+				throw new System.ArgumentException("A Request URI is required.");
 			}
 
 			HttpResponse smartsheetResponse = new HttpResponse();
@@ -183,19 +187,19 @@ namespace Smartsheet.Api.Internal.Http
 			// Create HTTP request based on the smartsheetRequest request Type
 			if (HttpMethod.GET == smartsheetRequest.Method)
 			{
-				 restRequest = new RestRequest(smartsheetRequest.Uri, Method.GET);
+				restRequest = new RestRequest(smartsheetRequest.Uri, Method.GET);
 			}
 			else if (HttpMethod.POST == smartsheetRequest.Method)
 			{
-				 restRequest = new RestRequest(smartsheetRequest.Uri, Method.POST);
+				restRequest = new RestRequest(smartsheetRequest.Uri, Method.POST);
 			}
 			else if (HttpMethod.PUT == smartsheetRequest.Method)
 			{
-				 restRequest = new RestRequest(smartsheetRequest.Uri, Method.PUT);
+				restRequest = new RestRequest(smartsheetRequest.Uri, Method.PUT);
 			}
 			else if (HttpMethod.DELETE == smartsheetRequest.Method)
 			{
-				 restRequest = new RestRequest(smartsheetRequest.Uri, Method.DELETE);
+				restRequest = new RestRequest(smartsheetRequest.Uri, Method.DELETE);
 			}
 			else
 			{
@@ -277,7 +281,7 @@ namespace Smartsheet.Api.Internal.Http
 				thisVersion = assembly.GetName().Version.ToString();
 				title = assembly.GetName().Name;
 			}
-			return "smartsheet-csharp-sdk("+title + ")/" + thisVersion + " " + Util.GetOSFriendlyName();
+			return "smartsheet-csharp-sdk(" + title + ")/" + thisVersion + " " + Util.GetOSFriendlyName();
 		}
 	}
 
