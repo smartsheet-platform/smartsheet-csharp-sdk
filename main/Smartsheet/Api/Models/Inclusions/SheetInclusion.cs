4a9483882d5c1f0529eff28985f4a001781b8acf
Tim Wells <timwells@westtek.com>
2017-08-28 16:48:52 -0700

    fix CellHistory deserialization

--- main/Smartsheet/Api/Models/Inclusions/SheetInclusion.cs
+++ main/Smartsheet/Api/Models/Inclusions/SheetInclusion.cs
@@ -35,6 +35,11 @@ namespace Smartsheet.Api.Models
 		/// <summary>
 		/// Includes the source for any sheet that was created from another sheet or template.
 		/// </summary>
-		SOURCE
+		SOURCE,
+
+		/// <summary>
+		/// Includes the sheet version for any sheet returned in this folder.
+		/// </summary>
+		SHEET_VERSION
 	}
 }
