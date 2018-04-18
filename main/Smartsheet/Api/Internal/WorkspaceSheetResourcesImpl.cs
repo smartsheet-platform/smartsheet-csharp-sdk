f520e45f2e4c5258dd6b28c8b004af5f69e39159
Kevin Fansler <kevin.fansler@smartsheet.com>
2018-04-18 14:29:22 -0700

    Update WorkspaceSheetResourcesImpl.cs

--- main/Smartsheet/Api/Internal/WorkspaceSheetResourcesImpl.cs
+++ main/Smartsheet/Api/Internal/WorkspaceSheetResourcesImpl.cs
@@ -9,7 +9,7 @@
 //        
 //            http://www.apache.org/licenses/LICENSE-2.0
 //        
-//    Unless required by applicable law or agreed To in writing, software
+//    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
@@ -46,16 +46,16 @@ namespace Smartsheet.Api.Internal
 		/// <summary>
 		/// <para>Create a sheet in given workspace.</para>
 		/// 
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /workspaces/{workspaceId}/Sheets</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /workspaces/{workspaceId}/Sheets</para>
 		/// </summary>
 		/// <param name="workspaceId"> the workspace Id </param>
-		/// <param name="sheet"> the sheet To create </param>
+		/// <param name="sheet"> the sheet to create </param>
 		/// <returns> the created sheet </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		public virtual Sheet CreateSheet(long workspaceId, Sheet sheet)
 		{
@@ -63,18 +63,18 @@ namespace Smartsheet.Api.Internal
 		}
 
 		/// <summary>
-		/// <para>Creates a Sheet at the top-level of the specified Workspace, from the specified Template. </para>
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /workspaces/{workspaceId}/Sheets</para>
+		/// <para>Creates a sheet at the top-level of the specified workspace, from the specified template. </para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /workspaces/{workspaceId}/Sheets</para>
 		/// </summary>
 		/// <param name="workspaceId"> the workspace Id </param>
-		/// <param name="sheet"> the sheet To create </param>
-		/// <param name="includes"> used To specify the optional objects To include </param>
+		/// <param name="sheet"> the sheet to create </param>
+		/// <param name="includes"> used to specify the optional objects to include </param>
 		/// <returns> the created sheet </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		public virtual Sheet CreateSheetFromTemplate(long workspaceId, Sheet sheet, IEnumerable<TemplateInclusion> includes)
 		{
@@ -87,8 +87,8 @@ namespace Smartsheet.Api.Internal
 		}
 
 		/// <summary>
-		/// <para>Imports a Sheet at the top-level of the specified Workspace (CSV). </para>
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /workspaces/{workspaceId}/sheets/import</para>
+		/// <para>Imports a sheet at the top-level of the specified workspace (CSV). </para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /workspaces/{workspaceId}/sheets/import</para>
 		/// </summary>
 		/// <param name="workspaceId"> the workspace Id </param>
 		/// <param name="file"> path to the image file</param>
@@ -96,11 +96,11 @@ namespace Smartsheet.Api.Internal
 		/// <param name="headerRowIndex"></param>
 		/// <param name="primaryColumnIndex"></param>
 		/// <returns> the created sheet </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		public virtual Sheet ImportCsvSheet(long workspaceId, string file, string sheetName, int? headerRowIndex, int? primaryColumnIndex)
 		{
@@ -108,8 +108,8 @@ namespace Smartsheet.Api.Internal
 		}
 
 		/// <summary>
-		/// <para>Imports a Sheet at the top-level of the specified Workspace (XLSX). </para>
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /workspaces/{workspaceId}/sheets/import</para>
+		/// <para>Imports a sheet at the top-level of the specified workspace (XLSX). </para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /workspaces/{workspaceId}/sheets/import</para>
 		/// </summary>
 		/// <param name="workspaceId"> the workspace Id </param>
 		/// <param name="file"> path to the image file</param>
@@ -117,11 +117,11 @@ namespace Smartsheet.Api.Internal
 		/// <param name="headerRowIndex"></param>
 		/// <param name="primaryColumnIndex"></param>
 		/// <returns> the created sheet </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		public virtual Sheet ImportXlsSheet(long workspaceId, string file, string sheetName, int? headerRowIndex, int? primaryColumnIndex)
 		{
@@ -130,7 +130,7 @@ namespace Smartsheet.Api.Internal
 		}
 
 		/// <summary>
-		/// Private routine to import a Sheet of contentType into a Workspace
+		/// Private routine to import a sheet of contentType into a workspace
 		/// </summary>
 		/// <param name="workspaceId"></param>
 		/// <param name="file"></param>
