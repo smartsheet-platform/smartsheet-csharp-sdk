9c5fca99d19e60aeba170f751bfbcfcc330e37f4
Kevin Fansler <kevin.fansler@smartsheet.com>
2018-04-18 16:16:26 -0700

    Update SortCriterion.cs

--- main/Smartsheet/Api/Models/SortCriterion.cs
+++ main/Smartsheet/Api/Models/SortCriterion.cs
@@ -9,7 +9,7 @@
 //        
 //            http://www.apache.org/licenses/LICENSE-2.0
 //        
-//    Unless required by applicable law or agreed To in writing, software
+//    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
@@ -26,7 +26,7 @@ namespace Smartsheet.Api.Models
 	public class SortCriterion
 	{
 		/// <summary>
-		/// The column ID to sort on
+		/// The column Id to sort on
 		/// </summary>
 		private long columnId;
 
@@ -36,9 +36,9 @@ namespace Smartsheet.Api.Models
 		private SortDirection direction;
 
 		/// <summary>
-		/// Get the column ID of the column to sort on
+		/// Get the column Id of the column to sort on
 		/// </summary>
-		/// <returns> the column ID </returns>
+		/// <returns> the column Id </returns>
 		public long ColumnId
 		{
 			get { return columnId; }
