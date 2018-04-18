f668ce7cc9dea5b4e1121195c5fd29803d46fce6
Kevin Fansler <kevin.fansler@smartsheet.com>
2018-04-18 16:28:25 -0700

    Update SheetDiscussionResources.cs

--- main/Smartsheet/Api/SheetDiscussionResources.cs
+++ main/Smartsheet/Api/SheetDiscussionResources.cs
@@ -9,7 +9,7 @@
 //        
 //            http://www.apache.org/licenses/LICENSE-2.0
 //        
-//    Unless required by applicable law or agreed To in writing, software
+//    Unless required by applicable law or agreed to in writing, software
 //    distributed under the License is distributed on an "AS IS" BASIS,
 //    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //    See the License for the specific language governing permissions and
@@ -22,103 +22,103 @@ using System.Collections.Generic;
 namespace Smartsheet.Api
 {
 	/// <summary>
-	/// <para>This interface provides methods To access Discussion resources that are associated To a sheet object.</para>
+	/// <para>This interface provides methods to access discussion resources that are associated to a Sheet object.</para>
 	/// 
 	/// <para>Thread Safety: Implementation of this interface must be thread safe.</para>
 	/// </summary>
 	public interface SheetDiscussionResources
 	{
 		/// <summary>
-		/// <para>Creates a new Discussion on a Sheet.</para>
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /sheets/{sheetId}/discussions</para>
+		/// <para>Creates a new discussion on a sheet.</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /sheets/{sheetId}/discussions</para>
 		/// </summary>
-		/// <param name="sheetId"> the id of the sheet </param>
+		/// <param name="sheetId"> the Id of the sheet </param>
 		/// <param name="discussion"> the discussion to add </param>
 		/// <returns> the created discussion </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		Discussion CreateDiscussion(long sheetId, Discussion discussion);
 
 		/// <summary>
-		/// <para>Creates a new Discussion attached with an Attachment on a Sheet.</para>
-		/// <para>It mirrors To the following Smartsheet REST API method: POST /sheets/{sheetId}/discussions</para>
+		/// <para>Creates a new discussion with an attachment on a sheet.</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: POST /sheets/{sheetId}/discussions</para>
 		/// </summary>
-		/// <param name="sheetId"> the id of the sheet </param>
+		/// <param name="sheetId"> the Id of the sheet </param>
 		/// <param name="discussion"> the discussion to add </param>
 		/// <param name="file"> the file path </param>
 		/// <param name="fileType"> the file type, can be null </param>
 		/// <returns> the created discussion </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		Discussion CreateDiscussionWithAttachment(long sheetId, Discussion discussion, string file, string fileType);
 
 		/// <summary>
-		/// <para>Delete a discussion</para>
-		/// <para>It mirrors To the following Smartsheet REST API method: DELETE /sheets/{sheetId}/discussions/{discussionId}</para>
+		/// <para>Deletes a discussion</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: DELETE /sheets/{sheetId}/discussions/{discussionId}</para>
 		/// </summary>
-		/// <param name="sheetId"> the sheetId </param>
-		/// <param name="discussionId"> the discussionId</param>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <param name="sheetId"> the sheet Id </param>
+		/// <param name="discussionId"> the discussion Id</param>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		void DeleteDiscussion(long sheetId, long discussionId);
 
 		/// <summary>
-		/// <para>Gets a list of all Discussions associated with the specified Sheet (both sheet-level discussions and row-level discussions).</para>
-		/// <para>It mirrors To the following Smartsheet REST API method: GET /sheets/{sheetId}/discussions</para>
+		/// <para>Gets a list of all discussions associated with the specified sheet (both sheet-level discussions and row-level discussions).</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: GET /sheets/{sheetId}/discussions</para>
 		/// <remarks>This operation supports pagination of results. For more information, see Paging.</remarks>
 		/// </summary>
 		/// <param name="sheetId"> the sheet Id </param>
-		/// <param name="include">elements to include in response</param>
+		/// <param name="include">elements to include in the response</param>
 		/// <param name="paging">the pagination</param>
-		/// <returns> list of all Discussions </returns>
-		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
+		/// <returns> list of all discussions </returns>
+		/// <exception cref="System.InvalidOperationException"> if any argument is null or an empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		PaginatedResult<Discussion> ListDiscussions(long sheetId, IEnumerable<DiscussionInclusion> include, PaginationParameters paging);
 
 
 		/// <summary>
-		/// <para>Gets the Discussion specified in the URL.</para>
-		/// <para>It mirrors To the following Smartsheet REST API method: GET /sheets/{sheetId}/discussions/{discussionId}</para>
+		/// <para>Gets the discussion specified in the URL.</para>
+		/// <para>Mirrors to the following Smartsheet REST API method: GET /sheets/{sheetId}/discussions/{discussionId}</para>
 		/// </summary>
 		/// <param name="sheetId"> the Id of the sheet </param>
 		/// <param name="discussionId"> the ID of the discussion </param>
 		/// <returns> the discussion </returns>
 		/// <exception cref="System.InvalidOperationException"> if any argument is null or empty string </exception>
 		/// <exception cref="InvalidRequestException"> if there is any problem with the REST API request </exception>
-		/// <exception cref="AuthorizationException"> if there is any problem with  the REST API authorization (access token) </exception>
+		/// <exception cref="AuthorizationException"> if there is any problem with the REST API authorization (access token) </exception>
 		/// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
-		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due To rate limiting) </exception>
+		/// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
 		/// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
 		Discussion GetDiscussion(long sheetId, long discussionId);
 
 		/// <summary>
 		/// <para>Return the DiscussionAttachmentResources object that provides access
-		/// To Attachment resources associated with Discussion resources.</para>
+		/// to attachment resources associated with discussion resources.</para>
 		/// </summary>
-		/// <returns> the attachment resources object </returns>
+		/// <returns> the DiscussionAttachmentResources object </returns>
 		DiscussionAttachmentResources AttachmentResources { get; }
 
 		/// <summary>
-		/// <para>Return the DiscussionCommentResources object that provides access
-		/// To Comment resources associated with Discussion resources.</para>
+		/// <para>Returns the DiscussionCommentResources object that provides access
+		/// to comment resources associated with discussion resources.</para>
 		/// </summary>
-		/// <returns> the comment resources object </returns>
+		/// <returns> the DiscussionCommentResources object </returns>
 		DiscussionCommentResources CommentResources { get; }
 	}
 }
