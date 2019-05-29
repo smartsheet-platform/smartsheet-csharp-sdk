48096e542a7f55bc13c816d00394bd807e9ef7fe
Tim Wells <timwells@westtek.com>
2019-05-28 18:08:19 -0700

    added project files for netstandard2.0
    tried to minimize changes in the event of a rollback.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+//    #[license]
+//    Smartsheet SDK for C#
+//    %%
+//    Copyright (C) 2014 Smartsheet
+//    %%
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//        
+//            http://www.apache.org/licenses/LICENSE-2.0
+//        
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//    %[license]
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Smartsheet C# SDK")]
+[assembly: AssemblyDescription("Library that uses C# to connect to Smartsheet services.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Smartsheet")]
+[assembly: AssemblyProduct("Smartsheet_csharp_sdk.Properties")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a Type in this assembly from 
+// COM, set the ComVisible attribute to true on that Type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("77f0da27-4da3-437b-ac15-82643cdf2fa6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.68.1.0")]
+[assembly: AssemblyFileVersion("2.68.1.0")]
