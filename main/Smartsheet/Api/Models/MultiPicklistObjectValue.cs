e468b64a24ac471802ea0af0a41ee277a9bd3e01
Tim Wells <timwells@westtek.com>
2019-11-08 12:09:20 -0800

    !cosmetic: update copyright

--- main/Smartsheet/Api/Models/MultiPicklistObjectValue.cs
+++ main/Smartsheet/Api/Models/MultiPicklistObjectValue.cs
@@ -1,7 +1,7 @@
 ﻿//    #[license]
 //    SmartsheetClient SDK for C#
 //    %%
-//    Copyright (C) 2018 SmartsheetClient
+//    Copyright (C) 2019 SmartsheetClient
 //    %%
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
